1b725d14e4826f46961e81a13a637b9e5f91e2c3
Rjack00 <rjackson77@comcast.net>
2024-10-29 16:18:46 -0600

    script.js updated
    added: status{}, messages{}, cash and price variables, started register function

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 const cashInput = document.getElementById('cash');
 const changeDueElement = document.getElementById('change-due');
 const purchaseBtn = document.getElementById('purchase-btn');
+const totalDue = document.getElementById("cost-amount");
 
-let price = 1.87;
+const status = {
+  open: "OPEN",
+  closed: "CLOSED",
+  insuff: "INSUFFICIENT_fUNDS"
+};
+
+let cash = cashInput.value;
+let price = totalDue.value;
+const messages = {
+  insufficientMessage: "Customer does not have enough money to purchase the item",
+  exactCash: "No change due - customer paid with exact cash"
+}
+
 let cid = [
   ['PENNY', 1.01],
   ['NICKEL', 2.05],
@@ -14,3 +27,8 @@ let cid = [
   ['TWENTY', 60],
   ['ONE HUNDRED', 100]
 ];
+
+
+const register = (cashAmnt) => {
+  
+}
