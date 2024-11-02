75fd1b72938362a39818a7f4d759de53e5777208
Rjack00 <rjackson77@comcast.net>
2024-11-01 19:28:10 -0600

    elements.js created
    created to hold document.getElements to modulize app and make tesing easier

---
+++ elements.js
@@ -0,0 +1,7 @@
+const totalDue = document.getElementById("cost-amount");
+const cashInput = document.getElementById('cash');
+const changeTotalElement = document.getElementById("change-total");
+const purchaseBtn = document.getElementById('purchase-btn');
+const changeDueElement = document.getElementById('change-due');
+
+export {totalDue, cashInput, changeTotalElement, purchaseBtn, changeDueElement};
