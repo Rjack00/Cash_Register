26c595d77fa2455c6eef0950765fed43d094e6f0
Rjack00 <rjackson77@comcast.net>
2024-11-05 14:03:25 -0700

    elements.js updated
    refactored element variables to be in a function and returned as an object

--- elements.js
+++ elements.js
@@ -1,7 +1,12 @@
-const totalDue = document.getElementById("cost-amount");
-const cashInput = document.getElementById('cash');
-const changeTotalElement = document.getElementById("change-total");
-const purchaseBtn = document.getElementById('purchase-btn');
-const changeDueElement = document.getElementById('change-due');
+console.log("Test Elements______");
+const getDomElements = () => {
+    return {
+        totalDue: document.getElementById("cost-amount"),
+        cashInput: document.getElementById("cash"),
+        changeTotalElement: document.getElementById("change-total"),
+        purchaseBtn: document.getElementById('purchase-btn'),
+        changeDueElement: document.getElementById('change-due')
+    };
+}
 
-export {totalDue, cashInput, changeTotalElement, purchaseBtn, changeDueElement};
+export { getDomElements };
