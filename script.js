53ecf54d990d50ef2ee11b8c1a84b3433ebc2202
Rjack00 <rjackson77@comcast.net>
2024-11-14 18:25:46 -0700

    script.js updated
    working to get exact change to the penny

--- script.js
+++ script.js
@@ -31,16 +31,19 @@ let cid = [
   ['ONE HUNDRED', 100]
 ];
 
+// reversing cid to place larger values first
 let cidReversed = cid.reverse();
+// changed dollars to cents to avoid floating-point precision issues
+for(const element of cidReversed) {
+  element[1] = Math.round(element[1] * 100);
+}
 
 const denominationNames = ["ONE HUNDRED","TWENTY","TEN","FIVE","ONE","QUARTER","DIME","NICKLE","PENNY"];
 
 const denominations = [10000, 2000, 1000, 500, 100, 25, 10, 5, 1];
 
 let cidTotal = () => {
-  const cidCopyM100 = [...cidReversed];
-  cidCopyM100.forEach(item => item[1] = Math.round(item[1] * 100));
-  return cidCopyM100.reduce((a, b) => a + Number(b[1]), 0) / 100;
+  return cidReversed.reduce((a, b) => a + Number(b[1]), 0);
 }
 
 const updateStatus = (registerStatus) => {
@@ -62,14 +65,14 @@ const updateUI = () => {
     changeDueElement.innerHTML += `<p>${subArr[0]}: $${subArr[1]}</p>`;
   }
 }
-
+console.log('cid1',cidReversed);
 // TODO register():
 // - add STATUS: to messages
 // - innerHTML for changeDueElement
 // - innerHTML for changeTotalElement
 // - innerHTML for changeInDrawer
 const register = (cashAmount, price) => {
-
+  
   if(!price) {
     alert("Enter amount due");
     return;
@@ -92,10 +95,11 @@ const register = (cashAmount, price) => {
     registerMessage = messages.exactCash;
     return;
   }
-  
+  // need to get exact change; currently can't get all 0's when 1 - 334.41
  // making change
   while(change > 0) {
     for (let i = 0; i < denominations.length; i++) {
+      console.log(`cidTotal(): ${cidTotal()}, change ${change}`);
       if (cidTotal() < change) {
         registerStatus = `insufficient`;
         return;
@@ -105,20 +109,28 @@ const register = (cashAmount, price) => {
       }
       if(denominations[i] <= change) {
         change -= denominations[i];
-        cidReversed[i][1] -= denominations[i] / 100;
+        cidReversed[i][1] -= denominations[i];
         
         if (changeDue.some(subArray => subArray.includes(denominationNames[i]))) {
           for(const denomination of changeDue) {
               if(denomination[0] === denominationNames[i]) {
-                denomination[1] += denominations[i] / 100;
+                denomination[1] += denominations[i];
               }
           }
         } else {
-        changeDue.push([denominationNames[i], denominations[i] / 100]);
+        changeDue.push([denominationNames[i], denominations[i]]);
         }
         break;
       }
     }
+  }
+  // changing denominations of changeDue to dollars
+  for(const element of changeDue) {
+    element[1] = Math.round(element[1] / 100);
+  }
+  // changing denominations of cidReversed back to dollars
+  for(const element of cidReversed) {
+    element[1] = Math.round(element[1] / 100);
   }
   console.log("changeDue: ", changeDue);
   console.log("cidReversed: ", cidReversed);
